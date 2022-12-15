d51029ad5ac795e5bb35d42033f358cc736dd859
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-14 21:31:57 -0600

    env

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-const Dotenv = require('dotenv-webpack');
+// const Dotenv = require('dotenv-webpack');
 
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new Dotenv(
-      {systemvars: true})
-    ]
-  }
+  transpileDependencies: true
+  // configureWebpack: {
+  //   plugins: [
+  //     new Dotenv(
+  //     {systemvars: true})
+  //   ]
+  // }
 })
 
 
